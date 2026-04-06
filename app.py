326413817a4c26f286388fa616137eee75e6a5d8
meghanakondabathula <kmeghana293@gmail.com>
2026-04-07 02:14:54 +0530

    hug

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import numpy as np
 import gradio as gr
 from torchvision.models import efficientnet_b4, vit_b_16
-import gdown
 import os
 import gdown
 
@@ -13,4 +12,6 @@ def hello(name):
 
 demo = gr.Interface(fn=hello, inputs="text", outputs="text")
 
-demo.launch(server_name="0.0.0.0", server_port=7860)
+demo.launch()
+
+
